641b2f5a084f322636363c7f5f41dab107509743
Gabriele Petronella <gabriele@buildo.io>
2023-10-17 15:36:41 +0200

    Step 3 - React Query

--- src/Trips.tsx
+++ src/Trips.tsx
@@ -1,19 +1,20 @@
-import { useEffect, useState } from "react";
 import { getTrips } from "./api";
 import { Trip } from "./Trip";
 import * as styles from "./Trips.css";
-import * as models from "./models";
+import { useQuery } from "@tanstack/react-query";
 
 export function Trips() {
-  const [trips, setTrips] = useState<models.Trip[]>([]);
-
-  useEffect(() => {
-    getTrips().then(setTrips);
-  }, []);
+  const {
+    data: trips,
+    isError,
+    isLoading,
+  } = useQuery({ queryKey: ["trips"], queryFn: getTrips });
 
   return (
     <div className={styles.trips}>
-      {trips.map((trip) => (
+      {isError && <div>Error while fetching trips!!</div>}
+      {isLoading && <div>Loading...</div>}
+      {trips?.map((trip) => (
         <Trip key={trip.id} {...trip} />
       ))}
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MainLayout } from './MainLayout'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MainLayout } from "./MainLayout";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 0 },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MainLayout />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <MainLayout />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
