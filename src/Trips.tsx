c107f13d30442197ed3302371bc4317e75a8d44c
Gabriele Petronella <gabriele@buildo.io>
2023-10-18 15:02:19 +0200

    Use status to render trips

--- src/Trips.tsx
+++ src/Trips.tsx
@@ -4,19 +4,21 @@ import * as styles from "./Trips.css";
 import { useQuery } from "@tanstack/react-query";
 
 export function Trips() {
-  const {
-    data: trips,
-    isError,
-    isLoading,
-  } = useQuery({ queryKey: ["trips"], queryFn: getTrips });
+  const { data: trips, status } = useQuery({
+    queryKey: ["trips"],
+    queryFn: getTrips,
+  });
 
-  return (
-    <div className={styles.trips}>
-      {isError && <div>Error while fetching trips!!</div>}
-      {isLoading && <div>Loading...</div>}
-      {trips?.map((trip) => (
-        <Trip key={trip.id} {...trip} />
-      ))}
-    </div>
-  );
+  const renderTrips = (): React.ReactNode => {
+    switch (status) {
+      case "pending":
+        return <div>Loading...</div>;
+      case "error":
+        return <div>Error while fetching trips!!</div>;
+      case "success":
+        return trips?.map((trip) => <Trip key={trip.id} {...trip} />);
+    }
+  };
+
+  return <div className={styles.trips}>{renderTrips()}</div>;
 }
