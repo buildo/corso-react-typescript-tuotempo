7b405299012aeaf4d6ec7f7a533dcd6e06e354d5
Gabriele Petronella <gabriele@buildo.io>
2023-10-18 15:06:53 +0200

    Add ts-pattern

--- src/Trips.tsx
+++ src/Trips.tsx
@@ -1,24 +1,26 @@
+import { match } from "ts-pattern";
 import { getTrips } from "./api";
 import { Trip } from "./Trip";
 import * as styles from "./Trips.css";
 import { useQuery } from "@tanstack/react-query";
 
 export function Trips() {
-  const { data: trips, status } = useQuery({
+  const tripsQuery = useQuery({
     queryKey: ["trips"],
     queryFn: getTrips,
   });
 
-  const renderTrips = (): React.ReactNode => {
-    switch (status) {
-      case "pending":
-        return <div>Loading...</div>;
-      case "error":
-        return <div>Error while fetching trips!!</div>;
-      case "success":
-        return trips?.map((trip) => <Trip key={trip.id} {...trip} />);
-    }
-  };
-
-  return <div className={styles.trips}>{renderTrips()}</div>;
+  return (
+    <div className={styles.trips}>
+      {match(tripsQuery)
+        .with({ status: "pending" }, () => <div>Loading...</div>)
+        .with({ status: "error" }, () => (
+          <div>Error while fetching trips!!</div>
+        ))
+        .with({ status: "success" }, ({ data: trips }) =>
+          trips.map((trip) => <Trip key={trip.id} {...trip} />)
+        )
+        .exhaustive()}
+    </div>
+  );
 }
